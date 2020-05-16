2b0eeea3c28125d3b843edf9aecd06fe0b1e865b
Kevin Hall <kevinmhall8@gmail.com>
2020-05-16 06:41:27 -0700

    fixing bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,75 +1,77 @@
 const FILES_TO_CACHE = [
-  "/", 
-  "/index.html", 
-  "index.js", 
-  "db.js",
-  "styles.css", 
-  "icons/icon-192x192.png", 
-  "icons/icon-512x512.png"
+  "/",
+  "/index.html",
+  "/index.js", 
+  "/styles.css",
+  "/db.js",
+  "/icons/icon-192x192.png", 
+  "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = "static-cache-v2";
+
+const CACHE_NAME =  "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
-self.addEventListener("install", function(evt) {
-    evt.waitUntil(
+// install 
+console.log(self);
+self.addEventListener("install", function(evt){
+  evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
+          console.log("Your files were pre-cached successfully");
+          return cache.addAll(FILES_TO_CACHE);
       })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  // activate
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
       caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
+          return Promise.all(
+              keyList.map(key => {
+                  if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                      console.log("Removing old cache data", key);
+                      return caches.delete(key);
+                  }
+              })
+          );
       })
-    );
-  
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
+  );
+
+  self.clients.claim();
+})
+
+// fetch
+
+self.addEventListener("fetch", function(evt){
+  if (evt.request.url.includes("/api/")) {
       evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
+          caches.open(DATA_CACHE_NAME).then(cache => {
+              return fetch(evt.request)
+              .then(response => {
+                  if (response.status === 200) {
+                      cache.put(evt.request.url, response.clone());
+                  }
+
+                  return response;
+              })
+              .catch(err => {
+                  return cache.match(evt.request);
+              });
+          }).catch(err => console.log(err))
       );
-  
+      
       return;
-    }
-  
-    evt.respondWith(
+  }
+
+  evt.respondWith(
       caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
-        });
+          return cache.match(evt.request).then(response => {
+              return response || fetch(evt.request);
+          });
       })
-    );
-  });
+  );
+});
